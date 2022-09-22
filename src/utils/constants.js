033509b8b4ec70860863f31ffff1ac653fc75288
yash <yashshekhar2934@gmail.com>
2022-09-22 01:43:19 -0700

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://w7.pngwing.com/pngs/71/189/png-transparent-vidcon-us-youtube-logo-advertising-television-youtube-television-text-trademark-thumbnail.png';
+export const demoChannelUrl = 'https://www.youtube.com/c/NatGeo';
+export const demoVideoUrl = 'https://www.youtube.com/watch?v=brE21SBO2j8';
+export const demoChannelTitle = 'Video Sharing Application';
+export const demoVideoTitle = 'An application made with React JS, Rapid API & Material UI 5';
+
